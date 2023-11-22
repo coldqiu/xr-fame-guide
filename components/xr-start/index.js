757e978018fa60c3c28dfe0d0ee9a05bb963d2f9
xingxing <you@example.com>
2023-11-22 18:12:14 +0800

    .

--- components/xr-start/index.js
+++ components/xr-start/index.js
@@ -19,6 +19,20 @@ Component({
    * 组件的方法列表
    */
   methods: {
-
+    // handleReady(e) {
+    //   console.log('handleReady: ', e);
+    // },
+    // handleARReady(e) {
+    //   console.log('handleARReady: ', e);
+    // },
+    // handleError(e) {
+    //   console.log('handleError: ', e);
+    // },
+    handleAssetsProgress: function ({detail}) {
+      console.log('assets progress', detail.value);
+    },
+    handleAssetsLoaded: function ({detail}) {
+      console.log('assets loaded', detail.value);
+    }
   }
 })

09a4a0d75b2dfdcccb631f4fcf984d3ad330d725
xingxing <you@example.com>
2023-11-23 18:09:30 +0800

    feat: 添加粒子页

---
+++ components/xr-particle/index.js
@@ -0,0 +1,38 @@
+// components/xr-start/index.js
+Component({
+
+  /**
+   * 组件的属性列表
+   */
+  properties: {
+
+  },
+
+  /**
+   * 组件的初始数据
+   */
+  data: {
+
+  },
+
+  /**
+   * 组件的方法列表
+   */
+  methods: {
+    // handleReady(e) {
+    //   console.log('handleReady: ', e);
+    // },
+    // handleARReady(e) {
+    //   console.log('handleARReady: ', e);
+    // },
+    // handleError(e) {
+    //   console.log('handleError: ', e);
+    // },
+    handleAssetsProgress: function ({detail}) {
+      console.log('assets progress', detail.value);
+    },
+    handleAssetsLoaded: function ({detail}) {
+      console.log('assets loaded', detail.value);
+    }
+  }
+})
--- pages/door/index.js
+++ pages/door/index.js
@@ -7,10 +7,12 @@ Page({
   data: {
 
   },
-  naviagteToPageIndex() {
+  naviagteToPage(e) {
+    console.log("e", e)
+    const page = e.target.dataset.page
+    console.log('page: ', page);
     wx.navigateTo({
-      url: '/pages/index/index'
+      url: `/pages/${page}/index`
     })
   }
-
 })
